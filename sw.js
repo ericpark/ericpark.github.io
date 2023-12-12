a6e4a1a78d309f5e2e431c9513f7ceb84eb70056
ericpark <4909815+ericpark@users.noreply.github.com>
2023-12-12 15:21:49 -0500

    added tech page

--- sw.js
+++ sw.js
@@ -12,9 +12,9 @@
  */
 
 importScripts("workbox-v3.6.3/workbox-sw.js");
-workbox.setConfig({ modulePathPrefix: "workbox-v3.6.3" });
+workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
 
-workbox.core.setCacheNameDetails({ prefix: "gatsby-plugin-offline" });
+workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
 
 workbox.skipWaiting();
 workbox.clientsClaim();
@@ -26,79 +26,62 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    url: "webpack-runtime-4ff5371e784785b38177.js"
+    "url": "webpack-runtime-4a1a9142f751f5798a9d.js"
   },
   {
-    url: "styles.1661457935623c552e44.css"
+    "url": "styles.2d424f9246987000e32e.css"
   },
   {
-    url: "styles-29147cbc04bbc833f6a0.js"
+    "url": "styles-29147cbc04bbc833f6a0.js"
   },
   {
-    url: "framework-8de36d3fd07627b19105.js"
+    "url": "framework-4e3de44279a007a5784e.js"
   },
   {
-    url: "app-42334df3f01bd1ab03f9.js"
+    "url": "app-6cdc64baabb8c92e5ac7.js"
   },
   {
-    url:
-      "component---node-modules-gatsby-plugin-offline-app-shell-js-786fca1931f6eac50ed9.js"
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-aea1aa21f4ba228d1ec7.js"
   },
   {
-    url: "offline-plugin-app-shell-fallback/index.html",
-    revision: "3eba89a22c2da9c890da83757bd3bb8c"
+    "url": "offline-plugin-app-shell-fallback/index.html",
+    "revision": "4f65035615c0c0ce0269d807b9cd73d2"
   },
   {
-    url: "polyfill-67ce08954519b26f9c4e.js"
+    "url": "polyfill-9aa1228a4d1d71414837.js"
   },
   {
-    url: "manifest.json",
-    revision: "b773e901ef06ac93343dc05e67540787"
+    "url": "manifest.json",
+    "revision": "b773e901ef06ac93343dc05e67540787"
   },
   {
-    url: "manifest.webmanifest",
-    revision: "b156b5b72ab0315b6001f84e692a1ef0"
+    "url": "manifest.webmanifest",
+    "revision": "678691feb7dfaaf7995eb54c3a555e04"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerRoute(
-  /(\.js$|\.css$|static\/)/,
-  workbox.strategies.cacheFirst(),
-  "GET"
-);
-workbox.routing.registerRoute(
-  /^https?:.*\page-data\/.*\/page-data\.json/,
-  workbox.strategies.networkFirst(),
-  "GET"
-);
-workbox.routing.registerRoute(
-  /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-  workbox.strategies.staleWhileRevalidate(),
-  "GET"
-);
-workbox.routing.registerRoute(
-  /^https?:\/\/fonts\.googleapis\.com\/css/,
-  workbox.strategies.staleWhileRevalidate(),
-  "GET"
-);
+workbox.routing.registerRoute(/(\.js$|\.css$|static\/)/, workbox.strategies.cacheFirst(), 'GET');
+workbox.routing.registerRoute(/^https?:.*\page-data\/.*\/page-data\.json/, workbox.strategies.networkFirst(), 'GET');
+workbox.routing.registerRoute(/^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https?:\/\/fonts\.googleapis\.com\/css/, workbox.strategies.staleWhileRevalidate(), 'GET');
 
 /* global importScripts, workbox, idbKeyval */
 
-importScripts(`idb-keyval-iife.min.js`);
+importScripts(`idb-keyval-iife.min.js`)
 
-const { NavigationRoute } = workbox.routing;
+const { NavigationRoute } = workbox.routing
 
 const navigationRoute = new NavigationRoute(async ({ event }) => {
-  let { pathname } = new URL(event.request.url);
-  pathname = pathname.replace(new RegExp(`^`), ``);
+  let { pathname } = new URL(event.request.url)
+  pathname = pathname.replace(new RegExp(`^`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
-  const resources = await idbKeyval.get(`resources:${pathname}`);
-  if (!resources || !(await caches.match(`/app-42334df3f01bd1ab03f9.js`))) {
-    return await fetch(event.request);
+  const resources = await idbKeyval.get(`resources:${pathname}`)
+  if (!resources || !(await caches.match(`/app-6cdc64baabb8c92e5ac7.js`))) {
+    return await fetch(event.request)
   }
 
   for (const resource of resources) {
@@ -106,27 +89,27 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     // network - that way we won't risk being in an inconsistent state with
     // some parts of the page failing.
     if (!(await caches.match(resource))) {
-      return await fetch(event.request);
+      return await fetch(event.request)
     }
   }
 
-  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`;
-  return await caches.match(offlineShell);
-});
+  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  return await caches.match(offlineShell)
+})
 
-workbox.routing.registerRoute(navigationRoute);
+workbox.routing.registerRoute(navigationRoute)
 
 const messageApi = {
   setPathResources(event, { path, resources }) {
-    event.waitUntil(idbKeyval.set(`resources:${path}`, resources));
+    event.waitUntil(idbKeyval.set(`resources:${path}`, resources))
   },
 
   clearPathResources(event) {
-    event.waitUntil(idbKeyval.clear());
-  }
-};
+    event.waitUntil(idbKeyval.clear())
+  },
+}
 
 self.addEventListener(`message`, event => {
-  const { gatsbyApi } = event.data;
-  if (gatsbyApi) messageApi[gatsbyApi](event, event.data);
-});
+  const { gatsbyApi } = event.data
+  if (gatsbyApi) messageApi[gatsbyApi](event, event.data)
+})
